a3f3451a02315aedc7be62f2a4201e9b6ded5655
Trevor Allen <trevorallen@Trevors-MacBook-Air.local>
2021-03-25 17:36:37 -0500

    started layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,76 @@
 import './App.css';
+import { Container } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import 'fontsource-roboto';
+
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      test
-    </div>
+    <Container className="App" maxWidth='lg'>
+      <Grid container spacing={1}>
+        <Grid item className='hero' xs={12} sm={12} md={12} lg={12}>
+          <Typography variant='h1'>T</Typography>
+          <Typography variant='h3'>is</Typography>
+          <Typography variant='h3'>for</Typography>
+          <Typography variant='h1'>Trevor</Typography>
+        </Grid>
+        <Grid item className='about-me' xs={12} sm={12} md={12} lg={6}>
+          <Typography variant='h1'>A</Typography>
+          <Typography variant='h3'>little</Typography>
+          <Typography variant='h3'>about</Typography>
+          <Typography variant='h1'>Me</Typography>
+        </Grid>
+        <Grid className='kid' item xs={12} sm={12} md={12} lg={6}>
+          <Typography variant='h2'>I'm a 90's Kid...</Typography>
+          <Typography>Pokemon</Typography>
+          <Typography>Hot Wheels</Typography>
+          <Typography>N64</Typography>
+          <Typography>Tomigachi...(remember those?)</Typography>
+          <Typography>Born and raised in California</Typography>
+          <Typography>Can call many different places "home", including abroad</Typography>
+        </Grid>
+        <Grid className='family' item xs={12} sm={12} md={12} lg={6}>
+          <Typography variant='h1'>F</Typography>
+          <Typography variant='h3'>is</Typography>
+          <Typography variant='h3'>for</Typography>
+          <Typography variant='h1'>Family</Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={6}>
+          Family images...
+        </Grid>
+        <Grid className='school' item xs={12} sm={12} md={12} lg={6}>
+          <Typography variant='h1'>S</Typography>
+          <Typography variant='h3'>is</Typography>
+          <Typography variant='h3'>for</Typography>
+          <Typography variant='h1'>School</Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={6}>
+          BYU-I / Careerfoundry
+        </Grid>
+        <Grid className='work' item xs={12} sm={12} md={12} lg={6}>
+          <Typography variant='h1'>W</Typography>
+          <Typography variant='h3'>is</Typography>
+          <Typography variant='h3'>for</Typography>
+          <Typography variant='h1'>Work</Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={6}>
+          Portfolio display of work
+        </Grid>
+        <Grid className='hmu' item xs={12} sm={12} md={12} lg={6}>
+          <Typography variant='h1'>H</Typography>
+          <Typography variant='h3'>is</Typography>
+          <Typography variant='h3'>for</Typography>
+          <Typography variant='h1'>HMU</Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={6}>
+          Email, LinkedIn, Github, Resume
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
